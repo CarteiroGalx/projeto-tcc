7f4688d38c67168f7d47baabe4fd7cd47205c960
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2025-03-11 22:32:37 -0300

    Importando arquivos .NET MVC

---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_TCC.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public int celular { get; set; }
+    }
+}
